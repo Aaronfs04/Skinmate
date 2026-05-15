1d75b983cde49188870c3df4df1f2faf2a208013
aaronfas <aaronf.suryanto@gmail.com>
2026-05-15 13:47:14 +0700

    base

--- Frontend/skinmate/src/Component/Landing.tsx
+++ Frontend/skinmate/src/Component/Landing.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './Landing.css'
+import '../style/Landing.css'
 
-export default function Landing() {
+export function Landing() {
   const [count, setCount] = useState(0)
 
   return (
--- Frontend/skinmate/src/Pages/HeroPage.tsx
+++ Frontend/skinmate/src/Pages/HeroPage.tsx
@@ -1,4 +1,4 @@
-import  Landing  from '../Landing.tsx'
+import { Landing } from '../Component/Landing.tsx'
 
 export default function Heropages() {
     return (
